517d97ac94fc055b0a4c2180c14787812402ff9d
gaopinghuang0 <gaopinghuang@gmail.com>
2021-01-13 22:13:21 -0500

    Support place models

--- ModelPickerApp/ContentView.swift
+++ ModelPickerApp/ContentView.swift
@@ -7,26 +7,90 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ContentView : View {
+    @State private var isPlacementEnabled = false
+    @State private var selectedModel: Model?
+    @State private var modelConfirmedForPlacement: Model?
+    
+    var models: [Model] = getModelFilenames()
+    
     var body: some View {
         ZStack(alignment: .bottom) {
-            ARViewContainer()
+            ARViewContainer(modelConfirmedForPlacement: $modelConfirmedForPlacement)
+            
+            if isPlacementEnabled {
+                PlacementButtonView(
+                    isPlacementEnabled: $isPlacementEnabled,
+                    selectedModel: $selectedModel,
+                    modelConfirmedForPlacement: $modelConfirmedForPlacement
+                )
+            } else {
+                ModelPickerView(isPlacementEnabled: $isPlacementEnabled, selectedModel: $selectedModel, models: models)
+            }
         }
     }
 }
 
+func getModelFilenames() -> [Model] {
+        // Dynamically get our model filenames
+        let fileManager = FileManager.default
+        
+        guard let path = Bundle.main.resourcePath, let files = try? fileManager.contentsOfDirectory(atPath: path) else {
+            return []
+        }
+        
+        var availableModels: [Model] = []
+        for filename in files where filename.hasSuffix("usdz") {
+            let modelName = filename.replacingOccurrences(of: ".usdz", with: "")
+            let model = Model(modelName: modelName)
+            availableModels.append(model)
+        }
+        
+        return availableModels
+}
+
 struct ARViewContainer: UIViewRepresentable {
-    
+    @Binding var modelConfirmedForPlacement: Model?
+
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
+        
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal, .vertical]
+        config.environmentTexturing = .automatic
+        
+        if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) {
+            config.sceneReconstruction = .mesh
+        }
+        
+        arView.session.run(config)
 
         return arView
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: ARView, context: Context) {
+        if let model = modelConfirmedForPlacement {
+            
+            if let modelEntity = model.modelEntity {
+                print("DEBUG: adding model to scene - \(model.name)")
+                
+                let anchorEntity = AnchorEntity(plane: .any)
+                anchorEntity.addChild(modelEntity.clone(recursive: true))
+                
+                uiView.scene.addAnchor(anchorEntity)
+            } else {
+                print("DEBUG: Unable to load modelEntity for \(model.name)")
+            }
+
+            DispatchQueue.main.async {
+                modelConfirmedForPlacement = nil
+            }
+        }
+    }
     
 }
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#  SwiftUI + RealityKit app  -- Pick 3D models
+#  SwiftUI + RealityKit app  -- Pick and Place 3D Models
 
 Following this tutorial: [The Ultimate SwiftUI + RealityKit Tutorial // Pick and Place Multiple 3D Models](https://www.youtube.com/watch?v=9R_G0EI-UoI&ab_channel=RyanKopinsky)
 
